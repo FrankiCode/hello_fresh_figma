627b2e9d74b139c4694b31c630afba4885ea26f4
FrankiCode <jaceknauka@gmail.com>
2025-03-03 19:12:23 +0400

    mock index changed

---
+++ src/Mock/index.js
@@ -0,0 +1,57 @@
+export const whyHelloFresh = [
+    {id:31, image: "/why-hellofresh-icn1.png.png", title: "No skimpin’ on the chicken!", description: "Or steak, or fish, or plant protein."},
+    {id:32, image: "/why-hellofresh-icn2.png.png", title: "No commitment whatsoever", description: "Skipping weeks or cancelling is super easy."},
+    {id:33, image: "/why-hellofresh-icn3.png.png", title: "The most 5-star reviews", description: "Our huge recipe selection wows week after week."},
+    {id:34, image: "/why-hellofresh-icn4.png.png", title: "Fresh and affordable", description: "Chef-created deliciousness from $7.49 per meal."}
+]
+
+
+export const whatInsideEachBox = [
+    {id: 41, text: "✓ Easy-to-follow recipes with clear nutritional info"},
+    {id: 42, text: "✓ High-quality ingredients sourced straight from the farm"},
+    {id: 43, text: "✓ Convenient meal kits that fit perfectly in the fridge"},
+    {id: 44, text: "✓ A fun cooking experience that makes you feel unstoppable"},
+    {id: 45, text: "✓ Innovative packaging designed to reduce waste"},
+]
+
+export const recipesCards = [
+    {id: 51, image: "/image1.png", title: "MEAT & VEGGIES", category: "OUR MOST POPULAR PLAN"},
+    {id: 52, image: "/image2.png", title: "MEAT & VEGGIES", category: "OUR MOST POPULAR PLAN"},
+    {id: 53, image: "/image3.png", title: "MEAT & VEGGIES", category: "OUR MOST POPULAR PLAN"},
+    {id: 54, image: "/image1.png", title: "MEAT & VEGGIES", category: "OUR MOST POPULAR PLAN"},
+    {id: 55, image: "/image1.png", title: "MEAT & VEGGIES", category: "OUR MOST POPULAR PLAN"},
+    {id: 56, image: "/image1.png", title: "MEAT & VEGGIES", category: "OUR MOST POPULAR PLAN"}
+]
+
+export const cookItLoveIt =[ 
+    {id: 61, image: "/InstagramPeople.jpeg.png", title: "My little chef helping me make dinner tonight and trying out some of his new kitchen utensils I got him!"},
+    {id: 62, image: "/dog.jpeg.png", title: "I’m helping out 🧑🏽‍🍳 #masterchef #bestboy"},
+    {id: 63, image: "/IgramFamilyDinner.jpeg.png", title: "Drew and the kids made mom dinner tonight! Drew and I had our doubts but WOW was it tasty!"},
+    {id: 64, image: "/kidscookingedited.jpg.png", title: "We had a cooking class yesterday and we had such an amazing time. 😁 They had such a great time working as a team and I can’t wait for our weekly cooking class 🙌🏻😁"}
+]
+
+
+export const moreQuestions = [
+    {id:71, question: "How does HelloFresh’s meal kit delivery service work?"},
+    {id:72, question: "Which food meal plans & recipes does HelloFresh offer?"},
+    {id:73, question: "How many times a week does HelloFresh deliver?"},
+    {id:74, question: "Does HelloFresh give nutrition & calorie information?"},
+    {id:75, question: "How do I recycle my HelloFresh box?"},
+    {id:76, question: "Why should I choose HelloFresh as my meal kit service?"},
+    {id:77, question: "How much does HelloFresh cost?"},
+    {id:78, question: "Does HelloFresh support a healthy lifestyle?"},
+    {id:79, question: "Can I skip a week of delivery?"}
+]
+
+
+export const footerTitles = [
+    {id: 100, title: "HelloFresh"},
+    {id: 101, title: "Our company"},
+    {id: 102, title: "Work with us"},
+    {id: 103, title: "HelloFresContact ush"},
+    {id: 104, title: "Download our app"}
+]
+
+export const footerLinks = [
+    {id: 110, link: ""},
+]
