2df9ea822cae7fbbcac1ccf9183fb739606d2cca
FrankiCode <jaceknauka@gmail.com>
2025-03-03 19:11:13 +0400

    main.jsx changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
